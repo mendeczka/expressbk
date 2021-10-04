0b7c5e9787d209653a787774b2bce3e5b03d3235
mendeczka <57705516+mendeczka@users.noreply.github.com>
2021-10-04 20:19:34 +0200

    maly update

--- routes/config.js
+++ routes/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: 'mongodb+srv://userAdmin:DkUYXZFKenPPKPAr@cluster0.xiqmu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  db:,
   keySession: ['KeyToSessions'],
   maxAgeSession: 24 * 60 * 60 * 1000,
 }
