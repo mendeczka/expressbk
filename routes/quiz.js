77415d3cd6391251178f01992dbfbe75f4985ff9
mendeczka <mendeczka@o2.pl>
2021-10-04 19:53:13 +0200

    quiz front

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('quiz', { title: 'Quiz' });
+  const show = !req.session.vote;
+
+  // new Quiz({ title: 'Pytanie 1', vote: 0 }).save();
+
+  Quiz.find({},(err, data) => {
+    // console.log(data)
+
+    let sum = 0; 
+    data.forEach((item) => {
+      sum += item.vote;
+    });
+
+    res.render('quiz', { title: 'Quiz', data, show, sum });
+  });
+ 
 });
 
+router.post('/', (req, res) => {
+  const id = req.body.quiz;
+
+  Quiz.findOne({_id: id},(err, data) => {
+    // console.log(data)
+    data.vote = data.vote + 1;
+    data.save((err) => {
+      req.session.vote = 1;
+
+      res.redirect('/quiz');
+    });  
+  });
+});
 
 
 module.exports = router;
